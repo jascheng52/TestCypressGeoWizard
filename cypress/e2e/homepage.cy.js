fc4386e9f5143e367e89c6921245137a5acd20e9
Alex Toulemonde <alexandre.toulemonde@gmail.com>
2022-12-04 13:56:07 -0500

    Clarified default test behaviour

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,5 +1,7 @@
 describe("Test the homepage", () => {
   it("Check if the homepage loads properly", () => {
+    // Make sure you set your baseUrl in cypress.config.ts
+    // Or by overwriting it through the command line
     cy.visit("/");
   });
 });
