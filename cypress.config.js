27019d2e758c494bc8b34db4891be07c398f4036
Alex Toulemonde <alexandre.toulemonde@gmail.com>
2022-12-04 14:13:22 -0500

    Clarified variable name

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const cypress = require("cypress");
 
 module.exports = {
-  projectId: "YOUR_PROJECT_ID",
+  projectId: "CYPRESS_PROJECT_ID",
   e2e: {
     baseUrl: "http://google.com", // <-- Your staging root URL goes here
     setupNodeEvents(on, config) {
